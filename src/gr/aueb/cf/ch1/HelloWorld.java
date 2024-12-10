9966522a4e2a7df06dbd5e3d27c9eb81a1757056
ilias Barbitsiotis <hbarbitsiotis@windowslive.com>
2024-12-10 20:01:20 +0200

    initial commint

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Prints the phrase 'Hello World'
+ * Prints the phrase 'My name is Ilias and I want to introduce myself in java coding.'
+ * in the standard output (console).
+ *
+ * @author Ilias
+ */
+public class HelloWorld {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My name is Ilias" + " and" + " I want to introduce myself in java coding.");
+
+    }
+}
